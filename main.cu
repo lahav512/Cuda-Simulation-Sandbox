6d6e30552e6a4ea1dea00c2328ac8928c21068ac
lahavs512 <lahavs512@gmail.com>
2023-10-23 00:07:32 +0300

    typo fixed

--- main.cu
+++ main.cu
@@ -1,48 +1,49 @@
 #include <iostream>
+#include <math.h>
 #include <cuda.h>
-
-using namespace std;
-
-__global__ void addIntsCUDA(int *a, int *b) {
-    a[0] += b[0];
+ 
+// CUDA kernel to add elements of two arrays
+__global__ void add(int n, float *x, float *y)
+{
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] = x[i] + y[i];
 }
-
-int main() {
-    int a = 5;
-    int b = 9;
-    // int res;
-    int *d_a, *d_b;
-
-    *d_a = a;
-    *d_b = b;
-
-    // Allocate memory for the kernel function in the GPU
-    cudaMallocManaged(&d_a, sizeof(int));
-    cudaMallocManaged(&d_b, sizeof(int));
-
-    // // Allocate memory for the kernel function in the GPU
-    // cudaMalloc(&d_a, sizeof(int));
-    // cudaMalloc(&d_b, sizeof(int));
-
-    // // Copy the memory from the CPU to the GPU
-    // cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
-
-    // Execute the kernel function
-    addIntsCUDA<<<1, 1>>>(d_a, d_b);
-
-    // // Copy the result from the GPU to the CPU
-    // cudaMemcpy(&a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
-
-    // Wait for GPU to finish before accessing on host
-    cudaDeviceSynchronize();
-
-    // Print the result
-    cout<<"The result is "<< a <<endl;
-
-    // Clear the GPU memory
-    cudaFree(d_a);
-    cudaFree(d_b);
-
-    return 0;
+ 
+int main(void)
+{
+  int N = 1<<20;
+  float *x, *y;
+ 
+  // Allocate Unified Memory -- accessible from CPU or GPU
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+ 
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+ 
+  // Launch kernel on 1M elements on the GPU
+  int blockSize = 256;
+  int numBlocks = (N + blockSize - 1) / blockSize;
+  add<<<numBlocks, blockSize>>>(N, x, y);
+ 
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+ 
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++) {
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  }
+  std::cout << "Max error: " << maxError << std::endl;
+ 
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+ 
+  return 0;
 }
