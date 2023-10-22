57c07e8abcb1d39e41f462015b877f7009bc6d5f
lahavs512 <lahavs512@gmail.com>
2023-10-22 23:24:23 +0300

    main.cu has been added. This is the first example of a cuda code.

---
+++ main.cu
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cuda.h>
+
+using namespace std;
+
+__global__ void addIntsCUDA(int *a, int *b) {
+    a[0] += b[0];
+}
+
+int main() {
+    int a = 5;
+    int b = 9;
+    int res;
+    int *d_a, *d_b;
+
+    // Allocate memory for the kernel function in the GPU
+    cudaMalloc(&d_a, sizeof(int));
+    cudaMalloc(&d_b, sizeof(int));
+
+    // Copy the memory from the CPU to the GPU
+    cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+    // Execute the kernel function
+    addIntsCUDA(d_a, d_b);
+
+    // Copy the result from the GPU to the CPU
+    cudaMemcpy(&res, d_a, sizeof(int), cudaMemcpyDeviceToHost);
+
+    // Print the result
+    cout<<"The result is "<< a <<endl;
+
+    // Clear the GPU memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+
+    return 0;
+}
