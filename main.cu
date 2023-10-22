ce76f9406adea765479f6cb9521beee6fac5bfc1
lahavs512 <lahavs512@gmail.com>
2023-10-23 00:42:59 +0300

    typo fixed

--- main.cu
+++ main.cu
@@ -3,41 +3,31 @@
 #include <cuda.h>
  
 // CUDA kernel to add elements of two arrays
-__global__ void add(int n, float *x, float *y) {
-    for (int i = 0; i < n; i += 1) {
-        y[i] = x[i] + y[i];
-    }
+__global__ void AplusB(int *res, int a, int b) {
+    res[threadIdx.x] = a + b + threadIdx.x;
 }
  
 int main(void)
 {
-    int N = 1;
-    float *x, *y;
+    int N = 1000;
+    int *res;
 
-    // Allocate Unified Memory -- accessible from CPU or GPU
-    cudaMallocManaged(&x, N*sizeof(float));
-    cudaMallocManaged(&y, N*sizeof(float));
+    // Allocate memory
+    cudaMallocManaged(&res, 1000 * sizeof(int));
 
-    // initialize x and y arrays on the host
-    for (int i = 0; i < N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-    }
+    // Execute kernel function
+    AplusB<<< 1, N >>>(res, 10, 100);
 
-    // Launch kernel on 1M elements on the GPU
-    add<<<1, 1>>>(N, x, y);
-
-    // Wait for GPU to finish before accessing on host
+    // Wait until GPU completes calculations
     cudaDeviceSynchronize();
 
     // Print result
-    for (int i = 0; i < N; i++) {
-        std::cout << "x + y = " << y[i] << std::endl;
+    for(int i = 0; i < N; i++) {
+        printf("%d: A+B = %d\n", i, res[i]);
     }
 
-    // Free memory
-    cudaFree(x);
-    cudaFree(y);
+    // Clear memory
+    cudaFree(res);
 
     return 0;
 }
